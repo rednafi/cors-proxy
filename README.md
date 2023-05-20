bbf2447989bca19ff3ae8240894f6e5fd387947f
rednafi <redowan.nafi@gmail.com>
2023-05-20 18:09:22 -0400

    Init

---
+++ README.md
@@ -0,0 +1,92 @@
+# cors-proxy
+
+Simple cors-proxy server built with [Cloudflare Workers][cloudflare-workers].
+
+## Prerequisites
+
+Install the [wrangler][wrangler] CLI with:
+
+```sh
+npm install -g wrangler
+```
+
+## Bootstrap
+
+To bootstrap the application for the first time, the following command was executed:
+
+```sh
+npm create cloudflare@latest
+```
+
+<details>
+<summary><strong>Complete CLI output...</strong></strong></summary>
+
+```
+using create-cloudflare version 2.0.7
+
+╭ Create an application with Cloudflare Step 1 of 3
+│
+├ Where do you want to create your application?
+│ dir cors-proxy
+│
+├ What type of application do you want to create?
+│ type "Hello World" script
+│
+├ Do you want to use TypeScript?
+│ typescript no
+│
+├ Copying files from "simple" template
+│
+╰ Application created
+
+╭ Installing dependencies Step 2 of 3
+│
+├ Installing dependencies
+│ installed via `npm install`
+│
+╰ Dependencies Installed
+
+╭ Deploy with Cloudflare Step 3 of 3
+│
+├ Do you want to deploy your application?
+│ yes deploying via `npm run deploy`
+│
+├ Logging into Cloudflare This will open a browser window
+│ allowed via `wrangler login`
+│
+├ Deploying your application
+│ deployed via `npm run deploy`
+│
+├  SUCCESS  View your deployed application at
+│  https://cors-proxy.rednafi.workers.dev (this may take a few mins)
+│
+│ Run the development server npm run dev
+│ Deploy your application npm run deploy
+│ Read the documentation https://developers.cloudflare.com/workers
+│ Stuck? Join us at https://discord.gg/cloudflaredev
+│
+╰ See you again soon!
+```
+</details>&nbsp;
+
+## Development
+
+* Clone the repo and head over the root directory.
+* Go to `cors-proxy`.
+* Make your changes to the `src/worker.js` file.
+* Run the linter:
+    ```sh
+    npm run lint
+    ```
+* Run the development server:
+    ```sh
+    npm run devserver
+    ```
+
+## Deployment
+
+GitHub Actions will deploy the service to Cloudflare Workers on each push to the `main`
+branch.
+
+[cloudflare-workers]: https://workers.cloudflare.com/
+[wrangler]: https://developers.cloudflare.com/workers/wrangler/
---
+++ cors-proxy/src/worker.js
@@ -0,0 +1,65 @@
+/**
+ * Welcome to Cloudflare Workers! This is your first worker.
+ *
+ * - Run `npm run dev` in your terminal to start a development server
+ * - Open a browser tab at http://localhost:8787/ to see your worker in action
+ * - Run `npm run deploy` to publish your worker
+ *
+ * Learn more at https://developers.cloudflare.com/workers/
+ */
+
+export default {
+  async fetch(request, env, ctx) {
+    // Extract method, url and headers from incoming request object.
+    const { method, url, headers } = request;
+
+    // Extract destination url from query string.
+    const destUrl = new URL(url).searchParams.get("url");
+
+    // If destination url is not present, return 400.
+    if (!destUrl) {
+      return new Response("Missing destination URL.", { status: 400 });
+    }
+
+    // If request method is OPTIONS, return CORS headers.
+    if (
+      method === "OPTIONS" &&
+      headers.has("Origin") &&
+      headers.has("Access-Control-Request-Method")
+    ) {
+      const responseHeaders = {
+        "Access-Control-Allow-Origin": headers.get("Origin"),
+        "Access-Control-Allow-Methods": "*", // Allow all methods
+        "Access-Control-Allow-Headers": headers.get("Access-Control-Request-Headers"),
+        "Access-Control-Max-Age": "86400",
+      };
+      return new Response(null, { headers: responseHeaders });
+    }
+
+    const proxyRequest = new Request(destUrl, {
+      method,
+      headers: {
+        ...headers,
+        Origin: "",
+      },
+    });
+
+    try {
+      const response = await fetch(proxyRequest);
+      const responseHeaders = new Headers(response.headers);
+      responseHeaders.set("Access-Control-Allow-Origin", "*");
+      responseHeaders.set("Access-Control-Allow-Credentials", "true");
+      responseHeaders.set("Access-Control-Allow-Methods", "*");
+
+      return new Response(response.body, {
+        status: response.status,
+        statusText: response.statusText,
+        headers: responseHeaders,
+      });
+    } catch (error) {
+      return new Response("Error occurred while fetching the resource.", {
+        status: 500,
+      });
+    }
+  },
+};
